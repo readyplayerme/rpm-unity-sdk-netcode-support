cbe7e6cd0b73be2bee9ec2683b6868ee7a988a56
Robin <1121080+rYuuk@users.noreply.github.com>
2023-06-28 14:08:52 +0200

    feat: add transfer prefab util

---
+++ Editor/TransferPrefab.cs
@@ -0,0 +1,34 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace ReadyPlayerMe.NetcodeSupport.Editor
+{
+    public static class TransferPrefab
+    {
+        [MenuItem("Ready Player Me/Transfer Photon Prefab")]
+        public static void Transfer()
+        {
+            string[] guids = AssetDatabase.FindAssets("t:prefab RPM_Netcode_Character");
+
+            if (guids.Length == 0)
+            {
+                Debug.Log("RPM_Netcode_Character prefab not found. Please reimport Netcode Support package.");
+            }
+            else
+            {
+                if (AssetDatabase.LoadAssetAtPath("Assets/Ready Player Me/Resources/RPM_Character.prefab", typeof(GameObject)))
+                {
+                    if (!EditorUtility.DisplayDialog("Warning", "RPM_Character prefab already exists. Do you want to overwrite it?", "Yes", "No"))
+                    {
+                        return;
+                    }
+                }
+
+                string path = AssetDatabase.GUIDToAssetPath(guids[0]);
+                AssetDatabase.CopyAsset(path, "Assets/Ready Player Me/Resources/RPM_Character.prefab");
+                Selection.activeObject = AssetDatabase.LoadAssetAtPath("Assets/Ready Player Me/Resources/RPM_Character.prefab", typeof(GameObject));
+                Debug.Log("Netcode prefab transferred to Assets/Ready Player Me/Resources/RPM_Character.prefab");
+            }
+        }
+    }
+}
