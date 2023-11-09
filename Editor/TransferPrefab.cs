5ba0e9c18d642a1e6224d343517ddfe227edeae8
Harrison Hough <harrisonhough4@gmail.com>
2023-11-09 15:26:20 +0200

    chore: small cleanup

--- Editor/TransferPrefab.cs
+++ Editor/TransferPrefab.cs
@@ -6,12 +6,13 @@ namespace ReadyPlayerMe.NetcodeSupport.Editor
 {
     public static class TransferPrefab
     {
-        private const string PREFAB_PATH = "Assets/Ready Player Me/Resources/RPM_Netcode_Character.prefab";
+        private const string NEW_PREFAB_PATH = "Assets/Ready Player Me/Resources/RPM_Netcode_Character.prefab";
+        private const string PREFAB_ASSET_NAME = "t:prefab RPM_Netcode_Character";
 
         [MenuItem("Ready Player Me/Transfer Netcode Prefab", priority = 34)]
         public static void Transfer()
         {
-            string[] guids = AssetDatabase.FindAssets("t:prefab RPM_Netcode_Character");
+            var guids = AssetDatabase.FindAssets(PREFAB_ASSET_NAME);
 
             if (guids.Length == 0)
             {
@@ -19,15 +20,15 @@ namespace ReadyPlayerMe.NetcodeSupport.Editor
             }
             else
             {
-                if (AssetDatabase.LoadAssetAtPath(PREFAB_PATH, typeof(GameObject)))
+                if (AssetDatabase.LoadAssetAtPath(NEW_PREFAB_PATH, typeof(GameObject)))
                 {
                     if (!EditorUtility.DisplayDialog("Warning", "RPM_Netcode_Character prefab already exists. Do you want to overwrite it?", "Yes", "No"))
                     {
                         return;
                     }
                 }
-                PrefabTransferHelper.TransferPrefabByGuid(guids[0], PREFAB_PATH);
-                Debug.Log($"Netcode prefab transferred to {PREFAB_PATH}");
+                PrefabTransferHelper.TransferPrefabByGuid(guids[0], NEW_PREFAB_PATH);
+                Debug.Log($"Netcode prefab transferred to {NEW_PREFAB_PATH}");
             }
         }
     }
