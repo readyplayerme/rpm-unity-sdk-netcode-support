f7998101f8e731a3b5b07d8883f5d9fff36c6f35
Robin <1121080+rYuuk@users.noreply.github.com>
2023-12-01 17:08:44 +0530

    feat: use core package event and clean up

--- Editor/DependencyInstaller.cs
+++ Editor/DependencyInstaller.cs
@@ -1,25 +1,36 @@
 using ReadyPlayerMe.Core.Editor;
-using ReadyPlayerMe.NetcodeSupport.Editor;
 using UnityEditor;
 
-public class DependencyInstaller
+public abstract class DependencyInstaller
 {
+    private const string NETCODE_SUPPORT_PACKAGE_NAME = "com.readyplayerme.netcodesupport";
+    private const string NETCODE_HELPER_PACKAGE = "https://github.com/Unity-Technologies/com.unity.multiplayer.samples.coop.git?path=/Packages/com.unity.multiplayer.samples.coop#main";
+    private const string DIALOG_MESSAGE = "This package depends on - com.unity.multiplayer.samples.coop. Do you want to install it?";
+
     [InitializeOnLoadMethod]
-    static void Initialize()
+    private static void Initialize()
     {
-        PackageImporterListener.OnPackageImported += OnPackageImported;
+        PackageManagerEventListener.OnPackageImported += OnPackageImported;
     }
 
     ~DependencyInstaller()
     {
-        PackageImporterListener.OnPackageImported -= OnPackageImported;
+        PackageManagerEventListener.OnPackageImported -= OnPackageImported;
     }
 
-    private static void OnPackageImported()
+    private static void OnPackageImported(string packageName)
     {
-        if (EditorUtility.DisplayDialog("Ready Player Me", "This package depends on - com.unity.multiplayer.samples.coop. Do you want to install it?", "OK", "Cancel"))
+        if (packageName != NETCODE_SUPPORT_PACKAGE_NAME)
         {
-            PackageManagerHelper.AddPackage("https://github.com/Unity-Technologies/com.unity.multiplayer.samples.coop.git?path=/Packages/com.unity.multiplayer.samples.coop#main");
+            return;
+        }
+
+        if (EditorUtility.DisplayDialog("Ready Player Me", DIALOG_MESSAGE, "OK", "Cancel"))
+        {
+            if (!PackageManagerHelper.IsPackageInstalled(NETCODE_HELPER_PACKAGE))
+            {
+                PackageManagerHelper.AddPackage(NETCODE_HELPER_PACKAGE);
+            }
         }
     }
 }
